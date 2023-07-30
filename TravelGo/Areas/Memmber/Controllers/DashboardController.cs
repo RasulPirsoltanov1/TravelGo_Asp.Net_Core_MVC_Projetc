24b4f6859ec7df23856924a7e0cdde6cb03df574
RasulPirsoltanov1 <7m0uvuf@code.edu.az>
2023-07-30 13:35:43 +0400

    29 iyul Reservatin 43

---
+++ TravelGo/Areas/Memmber/Controllers/DashboardController.cs
@@ -0,0 +1,26 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TravelGo.Areas.Memmber.Controllers
+{
+    [Area("memmber")]
+    [Route("[Area]/[Controller]/[action]")]
+    public class DashboardController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public DashboardController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.Username = $"{user.Name} {user.Surname}";
+            ViewBag.ImageUrl = user.ImageUrl;
+            return View();
+        }
+    }
+}
