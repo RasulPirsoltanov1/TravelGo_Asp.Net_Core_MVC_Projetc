dfa788ca76871bca4aec3d1996fbd7664a91096b
RasulPirsoltanov1 <7m0uvuf@code.edu.az>
2023-07-30 21:44:15 +0400

    29 iyul Reservatin 45

---
+++ TravelGo/ViewComponents/MemmberDashboard/_GuideList.cs
@@ -0,0 +1,15 @@
+﻿using BusinessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TravelGo.ViewComponents.MemmberDashboard
+{
+    public class _GuideList : ViewComponent
+    {
+        GuideManager _guideManager = new GuideManager(new DataAccessLayer.Concrete.EntityFramework.EfGuideDal());
+        public IViewComponentResult Invoke()
+        {
+            var guides = _guideManager.TGetList();
+            return View(guides);
+        }
+    }
+}
