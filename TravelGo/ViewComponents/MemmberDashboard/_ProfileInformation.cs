29b89bef07357c3bb39f9a3e2c088d527372f61f
RasulPirsoltanov1 <7m0uvuf@code.edu.az>
2023-07-30 19:40:32 +0400

    29 iyul Reservatin 44

---
+++ TravelGo/ViewComponents/MemmberDashboard/_ProfileInformation.cs
@@ -0,0 +1,25 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TravelGo.ViewComponents.MemmberDashboard
+{
+    public class _ProfileInformation : ViewComponent
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public _ProfileInformation(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.NameSurname = $"{user.Name} {user.Surname}";
+            ViewBag.phone = user.PhoneNumber;
+            ViewBag.email = user.Email;
+            return View();
+        }
+    }
+}
