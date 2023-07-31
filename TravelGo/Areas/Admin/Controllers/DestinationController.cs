d01b89c71117deda9780756094d5a1d3d7f1b68a
RasulPirsoltanov <7m0uvuf@code.edu.az>
2023-08-01 01:01:53 +0400

    31 july 48

--- TravelGo/Areas/Admin/Controllers/DestinationController.cs
+++ TravelGo/Areas/Admin/Controllers/DestinationController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TravelGo.Areas.Admin.Controllers
 {
@@ -6,9 +8,41 @@ namespace TravelGo.Areas.Admin.Controllers
     [Route("[area]/[controller]/[action]")]
     public class DestinationController : Controller
     {
+        DestinationManager _destinationManager = new DestinationManager(new DataAccessLayer.Concrete.EntityFramework.EfDestinationDal());
+        [HttpGet]
         public IActionResult Index()
         {
+            var values = _destinationManager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddDestination()
+        {
             return View();
+        }
+        [HttpPost]
+        public IActionResult AddDestination(Destination destination)
+        {
+            _destinationManager.TAdd(destination);
+            return View();
+        }
+        [HttpDelete]
+        public IActionResult DeleteDestinationById(int Id)
+        {
+            _destinationManager.TDelete(_destinationManager.TGetById(Id));
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public IActionResult UpdateDestinationById(int Id)
+        {
+            var values = _destinationManager.TGetById(Id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult UpdateDestinationById(Destination destination)
+        {
+            _destinationManager.TUpdate(destination);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- TravelGo/ViewComponents/Default/_Features.cs
+++ TravelGo/ViewComponents/Default/_Features.cs
@@ -4,13 +4,16 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace TravelGo.ViewComponents.Default
 {
-    public class _Features:ViewComponent
+    public class _Features : ViewComponent
     {
         public IViewComponentResult Invoke()
         {
             FeatureManager _featureManager = new FeatureManager(new EfFeatureDal());
             var values = _featureManager.TGetList();
-            return View(values);
+            if (values.Count != 0)
+                return View(values);
+            else
+                return View(values);
         }
     }
 }
