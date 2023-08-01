74c4fdc121541887334b8624baa93a74554d4829
RasulPirsoltanov <7m0uvuf@code.edu.az>
2023-08-01 19:56:15 +0400

    1 avgust 51

---
+++ TravelGo/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TravelGo.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("[area]/[controller]/[action]")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TravelGo/ViewComponents/AdminDashboard/_Card1Statistics.cs
@@ -0,0 +1,16 @@
+﻿using DataAccessLayer.Concrete.Contexts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TravelGo.ViewComponents.AdminDashboard
+{
+    public class _Card1Statistics:ViewComponent
+    {
+        Context context=new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = context.Destinations.Count();
+            ViewBag.v2 = context.Users.Count();
+            return View();
+        }
+    }
+}
