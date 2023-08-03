d0e9e57da923958503c0c0bc53767169416cf74e
RasulPirsoltanov <7m0uvuf@code.edu.az>
2023-08-04 00:50:43 +0400

    added Ajax Actions to city Controller

---
+++ TravelGo/Areas/Admin/Controllers/CityController.cs
@@ -0,0 +1,62 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TravelGo.Models;
+
+namespace TravelGo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("[area]/[controller]/[action]")]
+    public class CityController : Controller
+    {
+        IDestinationService _destinationService;
+
+        public CityController(IDestinationService destinationService)
+        {
+            _destinationService = destinationService;
+        }
+
+        List<CityClass> cityClasses = new List<CityClass>()
+        {
+            new CityClass()
+            {
+                CityCountry="USA",
+                CityId=1,
+                CityName="NewYork"
+            },
+            new CityClass()
+            {
+                CityCountry="Russia",
+                CityId=2,
+                CityName="Moscova"
+            },
+            new CityClass()
+            {
+                CityCountry="China",
+                CityId=3,
+                CityName="Bangok"
+            }
+        };
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CityList()
+        {
+            var json = JsonConvert.SerializeObject(_destinationService.TGetList());
+            return Json(json);
+        }
+        [HttpPost]
+        public IActionResult Add([FromBody]Destination destination)
+        {
+            _destinationService.TAdd(destination);  
+            return Json(JsonConvert.SerializeObject(destination));
+        }
+        public IActionResult GetById([FromQuery]int DestinationId)
+        {
+            var json = JsonConvert.SerializeObject(_destinationService.TGetById(DestinationId));
+            return Json(json);
+        }
+    }
+}
