ee67a1dae83ab40a2847efb51f8bf66b2da3aff3
RasulPirsoltanov <7m0uvuf@code.edu.az>
2023-08-05 21:25:26 +0400

    Added Rapid api's [Exchenge Azn, top 100 Movies]

---
+++ TravelGo/Areas/Admin/Controllers/ApiMovieController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace TravelGo.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("[area]/[controller]/[action]")]
+    public class ApiMovieController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://imdb-top-100-movies1.p.rapidapi.com/"),
+                Headers ={
+                          { "X-RapidAPI-Key", "8a0b4388afmsh7ff887cca690cdep151b55jsnbca9dc528df8" },
+                          { "X-RapidAPI-Host", "imdb-top-100-movies1.p.rapidapi.com" },
+                         },
+
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                return View(JsonConvert.DeserializeObject<List<Movie>>(body));
+            }
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Detail(string id)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://imdb-top-100-movies1.p.rapidapi.com/?id={id}"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "8a0b4388afmsh7ff887cca690cdep151b55jsnbca9dc528df8" },
+        { "X-RapidAPI-Host", "imdb-top-100-movies1.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                return View(JsonConvert.DeserializeObject<Movie>(body));
+            }
+        }
+    }
+    public class Movie
+    {
+        public string title { get; set; }
+        public string id { get; set; }
+        public List<List<string>> image { get; set; }
+        public string description { get; set; }
+        public int rank { get; set; }
+        public string rating { get; set; }
+        public string thumbnail { get; set; }
+        public int year { get; set; }
+    }
+
+}
---
+++ TravelGo/Areas/Admin/Controllers/ExchangeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.DotNet.MSIdentity.Shared;
+using Newtonsoft.Json;
+
+namespace TravelGo.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("[area]/[controller]/[action]")]
+    public class ExchangeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://booking-com.p.rapidapi.com/v1/metadata/exchange-rates?currency=AZN&locale=en-gb"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "8a0b4388afmsh7ff887cca690cdep151b55jsnbca9dc528df8" },
+        { "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                return View(JsonConvert.DeserializeObject<Root>(body));
+            }
+        }
+    }
+
+
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class ExchangeRate
+    {
+        public string currency { get; set; }
+        public string exchange_rate_buy { get; set; }
+    }
+
+    public class Root
+    {
+        public List<ExchangeRate> exchange_rates { get; set; }
+        public string base_currency_date { get; set; }
+        public string base_currency { get; set; }
+    }
+
+}
