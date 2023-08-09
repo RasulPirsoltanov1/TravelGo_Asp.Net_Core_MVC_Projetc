b70f8211885271b25eb741ca3e3838df92b94c09
RasulPirsoltanov <7m0uvuf@code.edu.az>
2023-08-09 13:19:29 +0400

    added SignalRApi Layer

---
+++ SignalRApi/DAL/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SignalRApi.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+        public DbSet<Visitor> Visitors { get; set; }
+
+    }
+}
---
+++ SignalRApi/DAL/Visitor.cs
@@ -0,0 +1,19 @@
+﻿namespace SignalRApi.DAL
+{
+    public enum ECity
+    {
+        Baki=1,
+        Ismayilli=2,
+        Gence=3,
+        Samaxi=4,
+        Mingecevir=5
+    }
+    public class Visitor
+    {
+        public int Id { get; set; }
+        public ECity City{ get; set; }
+        public int CityVisitCount{ get; set; }
+        public DateTime VisitDate { get; set; }
+
+    }
+}
