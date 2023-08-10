4bcc99cb559a8994de4fb5c0d0da701891e27f7b
RasulPirsoltanov <7m0uvuf@code.edu.az>
2023-08-10 11:59:53 +0400

    added Role management controller

---
+++ TravelGo/Areas/Admin/Controllers/RoleController.cs
@@ -0,0 +1,41 @@
+﻿using DTOLayer.DTOs.RoleDTOs;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace TravelGo.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("[area]/[controller]/[action]")]
+    public class RoleController : Controller
+    {
+        private RoleManager<AppRole> _roleManager;
+
+        public RoleController(RoleManager<AppRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _roleManager.Roles.ToListAsync());
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(CreateRoleDTO createRoleDTO)
+        {
+            AppRole appRole = new AppRole()
+            {
+                Name = createRoleDTO.Name,
+            };
+            await _roleManager.CreateAsync(appRole);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
